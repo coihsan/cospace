851e2de31047b880621f5686a85b49d567b3dfb7
ihsan.onlinefs <ihsan.onlinefs@gmail.com>
2024-06-04 20:17:27 +0000

    git push

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider"
+const font = DM_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "cospace",
+  description: "Automate Your Work With Fuzzie.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
