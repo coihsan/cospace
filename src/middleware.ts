512ce88cbc205a70f9c2dc69c0143c31784d187e
ihsan.onlinefs <ihsan.onlinefs@gmail.com>
2024-06-07 22:55:24 +0000

    gitpush

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+import { NextResponse } from 'next/server'
 const isPublicRoute = createRouteMatcher(
   [
     '/',
@@ -9,13 +9,19 @@ const isPublicRoute = createRouteMatcher(
   ]
 )
 const isProtectedRoute = createRouteMatcher([
-  '/api/auth/callback/discord',
+    '/api/auth/callback/discord',
+    '/dashboard',
+    '/connections',
+    '/settings',
+    '/workflows',
     '/api/auth/callback/notion',
     '/api/auth/callback/slack',
     '/api/flow',
     '/api/cron/wait',
 ])
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
