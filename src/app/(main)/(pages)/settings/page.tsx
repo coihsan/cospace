82958595a19b5e2776b8084575239fc3ede3937e
ihsan.onlinefs <ihsan.onlinefs@gmail.com>
2024-06-08 20:59:32 +0000

    gitpush

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -42,7 +42,7 @@ const Settings = async (props : Props) =>{
       }
     return(
         <div className='flex flex-col gap-4'>
-            <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg border-zinc-50/20">
+            <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg border-muted-foreground">
                 <span>Settings</span>
             </h1>
             <div className='flex flex-col gap-10 p-6'>
